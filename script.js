e4e23cbde4e9e5df961f3a455ca3edc1b7074e48
chamodperera <chamodperera87@gmail.com>
2020-10-25 17:07:04 +0530

    updated theme change

--- script.js
+++ script.js
@@ -526,27 +526,8 @@ async function totalCasesCharts() {
   });
 }
 
-const btn = document.querySelector(".btn-toggle");
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-const currentTheme = localStorage.getItem("theme");
-if (currentTheme == "dark") {
-  document.body.classList.toggle("dark-theme");
-} else if (currentTheme == "light") {
-  document.body.classList.toggle("light-theme");
+function changetheme() {
+  var element = document.body
+  element.classList.toggle("dark-theme");
+  
 }
-
-btn.addEventListener("click", function () {
-  if (prefersDarkScheme.matches) {
-    document.body.classList.toggle("light-theme");
-    var theme = document.body.classList.contains("light-theme")
-      ? "light"
-      : "dark";
-  } else {
-    document.body.classList.toggle("dark-theme");
-    var theme = document.body.classList.contains("dark-theme")
-      ? "dark"
-      : "light";
-  }
-  localStorage.setItem("theme", theme);
-});
